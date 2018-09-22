0c5dc7d773d2e0edb58acdffb17a935aba1d133f
Homer Zuo <homerzhm@gmail.com>
2018-09-22 01:04:22 -0400

    Initial Commit

---
+++ LittleLibrary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LittleLibrary
+//
+//  Created by Hongming Zuo on 2018-09-22.
+//  Copyright © 2018 Hongming Zuo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
